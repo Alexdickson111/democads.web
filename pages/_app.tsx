5116552299d0238a99933b12e669b963d7598776
alexdickson111 <alexdickson53@gmail.com>
2022-03-07 08:28:32 +0100

    NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import theme from '../styles/theme';
+import NavBar from '../components/NavBar';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
 	return (
@@ -14,6 +15,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 			</Head>
+			<NavBar />
 			<ThemeProvider theme={theme}>
 				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
