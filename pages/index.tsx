e3b026e41af692be79e0d40b97972c409e0a0bfc
Alex Dickson <adickson@eurofirms.es>
2022-03-09 13:20:22 +0100

    fixed spacings

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,42 +10,47 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
-		<Box height="1000px" px={10} justifyContent="center">
-			<Box position="absolute" ml="550px">
+		<Box height="1000px" px={10} flexDirection="column" justifyContent="center" gap={10} sx={{ display: 'flex' }}>
+			<Box position="absolute" top={0} right={0}>
 				<img src="/hero.svg" alt="next" width="396px" />
 			</Box>
-			<Box width="569px" py={10}>
+			<Box width="569px" pt={10}>
 				<Typography fontWeight="bold" gutterBottom variant="h2">
-					<Typography color="primary.main" display="inline" fontWeight="bold" variant="h2">Democratize </Typography> your creator's <Typography color="primary.main" display="inline" fontWeight="bold" variant="h2">ads</Typography> revenue
+					<Typography color="primary.main" display="inline" fontWeight="bold" variant="h2">
+						Democratize{' '}
+					</Typography>{' '}
+					your creator's{' '}
+					<Typography color="primary.main" display="inline" fontWeight="bold" variant="h2">
+						ads
+					</Typography>{' '}
+					revenue
 				</Typography>
 				<Typography variant="body1" fontSize="16px" gutterBottom width="400px">
-					Democads is a decentralized ads platform to use on your site and reward your content creators fairly by conecting them directly to the advertiser with no middleman
+					Democads is a decentralized ads platform to use on your site and reward your content creators fairly by
+					conecting them directly to the advertiser with no middleman
 				</Typography>
 			</Box>
 
-			<Grid columns={3}>
-				<Box flexDirection="column" justifyContent="center" width="auto">
-					<PublicTwoTone sx={{ fontSize: "70px", color: "primary.main" }} />
-					<Typography fontWeight="bold">
-						Nose
-					</Typography>
-					<Typography >
-						Lorem ipsum emet
-					</Typography>
-				</Box>
-				<Box flexDirection="column">
-					<PublicTwoTone sx={{ fontSize: "70px", color: "primary.main" }} />
-					<Typography fontWeight="bold">
-						Nose
-					</Typography>
-					<Typography >
-						Lorem ipsum emet
-					</Typography>
-				</Box>
-				<Box flexDirection="column">
-
-				</Box>
+			<Grid container width="100%" justifyContent="center" spacing={10}>
+				<Grid item flexDirection="column" justifyContent="center" width="auto">
+					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
+					<Typography fontWeight="bold">Nose</Typography>
+					<Typography>Lorem ipsum emet</Typography>
+				</Grid>
+				<Grid item flexDirection="column" justifyContent="center" width="auto">
+					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
+					<Typography fontWeight="bold">Nose</Typography>
+					<Typography>Lorem ipsum emet</Typography>
+				</Grid>
+				<Grid item flexDirection="column" justifyContent="center" width="auto">
+					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
+					<Typography fontWeight="bold">Nose</Typography>
+					<Typography>Lorem ipsum emet</Typography>
+				</Grid>
 			</Grid>
+			<Typography fontWeight="bold" gutterBottom variant="h4" textAlign="center">
+				How it works
+			</Typography>
 			<Stepper></Stepper>
 		</Box>
 	);
