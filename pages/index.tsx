29b913c372f27e11deb06a316bbede8d8db0d9bd
alexdickson111 <alexdickson53@gmail.com>
2022-03-11 18:59:51 +0100

    Changed texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
-		<Box px={4} flexDirection="column" justifyContent="center" gap={10} maxWidth="100%" sx={{ display: 'flex' }}>
+		<Box px={6} flexDirection="column" justifyContent="center" gap={10} maxWidth="100%" sx={{ display: 'flex' }}>
 			<Box maxWidth="569px" mt={10}>
 				<Typography fontWeight="bold" gutterBottom variant="h2">
 					<Typography color="primary.main" display="inline" fontWeight="bold" variant="h2">
@@ -29,25 +29,25 @@ const Home: NextPage = () => {
 					them directly to the advertiser with no middleman
 				</Typography>
 			</Box>
-			<Box position={{ xs: 'relative', md: 'absolute' }} top={0} right={0}>
+			<Box position={{ xs: 'relative', md: 'absolute' }} top={0} right={6}>
 				<img src="/hero.svg" alt="next" width="396px" />
 			</Box>
 
 			<Grid container width="100%" justifyContent="center" gap={10}>
-				<Grid item flexDirection="column" justifyContent="center" width="auto">
+				<Grid item flexDirection="column" justifyContent="center" maxWidth="200px" textAlign="center">
 					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
-					<Typography fontWeight="bold">Nose</Typography>
-					<Typography>Lorem ipsum emet</Typography>
+					<Typography fontWeight="bold">Higher income</Typography>
+					<Typography>No middlemen to take a part of your creator's income</Typography>
 				</Grid>
-				<Grid item flexDirection="column" justifyContent="center" width="auto">
+				<Grid item flexDirection="column" justifyContent="center" maxWidth="200px" textAlign="center">
 					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
-					<Typography fontWeight="bold">Nose</Typography>
-					<Typography>Lorem ipsum emet</Typography>
+					<Typography fontWeight="bold">Own your data</Typography>
+					<Typography>Data is stored on the blockchain so YOU can do whatever you want with it</Typography>
 				</Grid>
-				<Grid item flexDirection="column" justifyContent="center" width="auto">
+				<Grid item flexDirection="column" justifyContent="center" maxWidth="200px" textAlign="center">
 					<PublicTwoTone sx={{ fontSize: '70px', color: 'primary.main' }} />
-					<Typography fontWeight="bold">Nose</Typography>
-					<Typography>Lorem ipsum emet</Typography>
+					<Typography fontWeight="bold">Transparency</Typography>
+					<Typography>Blockchain technology is inherently transparent. So you can see where all the money went</Typography>
 				</Grid>
 			</Grid>
 			<Typography fontWeight="bold" gutterBottom variant="h4" textAlign="center">
